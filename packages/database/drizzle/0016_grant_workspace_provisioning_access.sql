394fd60ecbceeff25719076ff3373d3e81b95818
luch91 <luchijudith@gmail.com>
2026-09-06 02:57:01 +0100

    fix(database): authorize workspace provisioning function

---
+++ packages/database/drizzle/0016_grant_workspace_provisioning_access.sql
@@ -0,0 +1,3 @@
+GRANT INSERT, UPDATE ON TABLE "tenants" TO hollis_migrator;
+GRANT INSERT, UPDATE ON TABLE "users" TO hollis_migrator;
+GRANT INSERT, UPDATE ON TABLE "tenant_memberships" TO hollis_migrator;
